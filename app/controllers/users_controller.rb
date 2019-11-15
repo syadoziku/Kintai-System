81a06235de12fd0b35e0174bfd7168b7047453b2
kawasaki <syadoziku@icloud.com>
2019-11-15 06:15:07 +0000

    11/15完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -91,15 +91,16 @@ class UsersController < ApplicationController
     end
     
     def admin_user
-      redirect_to root_url unless current_user.admin? 
+      unless current_user.admin? 
+      redirect_to(root_url)
       flash[:danger] = "権限がありません。"
+      end
     end
     
     def admin_or_correct_user
-      @user = User.find(params[:id])
       unless current_user?(@user) || current_user.admin?
+      redirect_to(root_url) 
       flash[:danger] = "権限がありません。"
-      redirect_to(root_url)
       end
     end
     
